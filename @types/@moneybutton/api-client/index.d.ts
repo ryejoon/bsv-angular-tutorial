9888a3bd53ba3fec7a18e4d07e8c7372e95bec9c
joonyeongpark <joonyeongpark@worksmobile.com>
2019-11-22 14:20:21 +0900

    moneybutton d.ts fix

--- @types/@moneybutton/api-client/index.d.ts
+++ @types/@moneybutton/api-client/index.d.ts
@@ -2,7 +2,7 @@
  * https://docs.moneybutton.com/docs/api-oauth-jsclient.html
  */
 export class MoneyButtonClient {
-  constructor(clientIdentifier: string, clientSecret: string);
+  constructor(clientIdentifier: string, clientSecret?: string);
 
   /**
    * @param permission ex> 'auth.user_identity:read'
@@ -10,7 +10,7 @@ export class MoneyButtonClient {
    */
   public requestAuthorization(permission: string, oAuthRedirectUrl: string);
 
-  public async handleAuthorizationResponse();
+  public handleAuthorizationResponse(): Promise<any>;
 
   public getRefreshToken(): any;
 }
